80899931de8f1b5bd323ca4301fbfc5ff021ff2f
Niyaz <niyaz@viamaguspro.com>
2022-11-07 09:12:38 +0530

    add path in side bar

--- src/container/SideBar.jsx
+++ src/container/SideBar.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import CardMembershipIcon from "@mui/icons-material/CardMembership";
 import HomeIcon from "@mui/icons-material/Home";
+import PrintIcon from "@mui/icons-material/Print";
+import React from "react";
 import { Link } from "react-router-dom";
 import * as routes from "../routes";
 const ITEMS = [
@@ -8,10 +11,23 @@ const ITEMS = [
     label: "Home",
     path: routes.LINK_CUSTOMER_SEARCH,
   },
+  {
+    icon: PrintIcon,
+    label: "Print Card",
+    path: routes.LINK_PRINT_CARD,
+  },
+  {
+    icon: CardMembershipIcon,
+    label: "View Card",
+    path: routes.LINK_VIEW_CARD,
+  },
 ];
 function SideBar(props) {
   return (
     <div className="">
+      <a href="" className="fw-bold d-flex align-items-center my-2 text-black ">
+        <ArrowBackIcon /> Back to Odisha One
+      </a>
       {ITEMS.map((data, index) => {
         return (
           <Link
